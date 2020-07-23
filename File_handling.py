d7c9372f341b8035992f98f333885f4e4f894517
AB-DUL <68370337+AB-DUL@users.noreply.github.com>
2020-07-23 13:24:45 +0530

    Add files via upload

---
+++ File_handling.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create a file
+book=open("book.txt",'w')
+book.close()
+
+
+# In[2]:
+
+
+# WRITE THE DATA
+book=open("book.txt",'w')
+book.write("Hi everyone, happy to see you again...!")
+book.close()
+
+
+# In[3]:
+
+
+# READ ENTIRE DATA FROM FILE
+book=open("book.txt",'r')
+book.read()
+
+
+# In[4]:
+
+
+from package import module as ap
+ap.add(235,45)
+
+
+# In[5]:
+
+
+from package import module as ap
+ap.add(235,45)
+
+
+# In[6]:
+
+
+from package import module
+module.add(235,45)
+
+
+# In[7]:
+
+
+from package import module as ap
+ap.add(235,45)
+
+
+# In[8]:
+
+
+from package import module1 as ap
+ap.add(235,45)
+
+
+# In[9]:
+
+
+from package import module as ap
+ap.add(235,45)
+ap.mul(235,45)
+ap.sub(235,45)
+ap.div(235,45)
+ap.mdiv(235,45)
+
+
+# In[10]:
+
+
+from package import module1 as ap
+ap.add(235,45)
+ap.multi(235,45)
+ap.sub(235,45)
+ap.div(235,45)
+ap.mdiv(235,45)
+
+
+# In[11]:
+
+
+from package import module1 as ap
+ap.add(235,45)
+ap.multi(235,45)
+ap.sub(235,45)
+ap.div(235,45)
+ap.mdiv(235,45)
+
+
+# In[12]:
+
+
+from package import module1 as ap
+ap.add(235,45)
+
+
+# In[13]:
+
+
+from package import module1 as ap
+ap.multi(235,45)
+
+
+# In[14]:
+
+
+from package import module1 as ap
+ap.sub(235,45)
+
+
+# In[15]:
+
+
+from package import module2 as ap
+ap.multi(235,45)
+
+
+# In[16]:
+
+
+from package import module3 as ap
+ap.sub(235,45)
+
+
+# In[17]:
+
+
+from package import module4 as ap
+ap.div(235,45)
+
+
+# In[19]:
+
+
+book=open("book.txt",'r')
+book.read()
+
+
+# In[18]:
+
+
+from package import module5 as ap
+ap.mdiv(235,45)
+
+
+# In[20]:
+
+
+book=open("book.txt",'r')
+book.readlines()
+
+
+# In[ ]:
+
+
+
+
